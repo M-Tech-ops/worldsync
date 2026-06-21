12450ddafb5a14c33878781f9d0797667805c2cb
ChillestOrange <chillestorange@gmail.com>
2026-06-21 22:22:29 +0530

    refactor: use LevelStorageAccess to resolve world path

--- src/main/java/io/github/chillestorange/mixin/client/WorldSaveMixin.java
+++ src/main/java/io/github/chillestorange/mixin/client/WorldSaveMixin.java
@@ -4,11 +4,11 @@ import io.github.chillestorange.client.ui.SyncingScreen;
 import io.github.chillestorange.config.WorldSyncConfig;
 import io.github.chillestorange.logging.WorldSyncLogger;
 import io.github.chillestorange.service.WorldSyncService;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.level.storage.LevelResource;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,8 +18,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.nio.file.Path;
-
 @Mixin(MinecraftServer.class)
 public class WorldSaveMixin {
 
@@ -28,7 +26,7 @@ public class WorldSaveMixin {
     protected LevelStorageSource.LevelStorageAccess storageSource;
 
     @Inject(method = "stopServer", at = @At("TAIL"))
-    private void worldsync$stopServerTail(CallbackInfo ci) {
+    private void worldsync$stopServer(CallbackInfo ci) {
         MinecraftServer server = (MinecraftServer) (Object) this;
 
         if (!(server instanceof IntegratedServer)) {
@@ -40,14 +38,11 @@ public class WorldSaveMixin {
             return;
         }
 
-        Path worldPath = FabricLoader.getInstance()
-                .getGameDir()
-                .resolve("saves")
-                .resolve(WorldSyncConfig.targetWorld());
+        var worldPath = storageSource.getLevelPath(LevelResource.ROOT);
 
-        WorldSyncLogger.info("Target world closed, starting upload sync: {}", levelId);
+        WorldSyncLogger.info("Target world closed, starting upload sync for world {}.", levelId);
 
-        Minecraft minecraft = Minecraft.getInstance();
+        var minecraft = Minecraft.getInstance();
         minecraft.execute(() -> minecraft.setScreen(new SyncingScreen()));
 
         WorldSyncService.runSyncCycle(
